e0e371e5c24a7f77bb7cdc7e4e94301ce268fb00
kokihata <hatakekoki@gmail.com>
2023-01-31 14:35:51 +0900

    slide show

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // navigation toggle
+  /*
+  $(".navbar-toggler-btn").click(function () {
+    $(".navbar-collapse").slideToggle(300);
+  });
+
+
+  // change navbar bg
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass("change-on-scroll", $(this).scrollTop() > $nav.height());
+  });
+*/
+
+  // header slider
+  $(".header-banner-wrapper").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+});
